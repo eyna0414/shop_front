49cebc893b4bc83917887b1347d7432842cb7715
eyna0414 <pey0414@gmail.com>
2025-03-19 17:46:47 +0900

    0319 파일첨부

--- src/components/practice/0317/UploadTest.jsx
+++ src/components/practice/0317/UploadTest.jsx
@@ -8,16 +8,16 @@ const UploadTest = () => {
   //자바로 데이터를 전달할 때 문자뿐만 아니라 파일 데이터도 가져간다는 것을 설정
   const fileConfig = {header:{'Content-type': 'multipart/form-data'}};
 
-  //첨부파일 데이터를 자바로 전달하기 위해서는 FormData()객체를 사용해야 함
-  //form 데이터 객체 생성 : 첨부파일, input 태그 등의 모든 데이터를 
-  //자바로 가져갈 수 있는 객체
-  const form = new FormData();
-  form.append('name', 'hong');
-  form.append('age', 20);
-  form.append('firstFile', firstFile);
-
   //파일 전송 axios
   const sendFile = () => {
+    //첨부파일 데이터를 자바로 전달하기 위해서는 FormData()객체를 사용해야 함
+    //form 데이터 객체 생성 : 첨부파일, input 태그 등의 모든 데이터를 
+    //자바로 가져갈 수 있는 객체
+    const form = new FormData();
+    form.append('bookName', 'hong');
+    form.append('bookPrice', 20);
+    form.append('firstFile', firstFile);
+
     //post() 메서드의 세번째 매개변수로 fileConfig를 전달(이거해야 파일 첨부 됨)
     axios.post(
       '/api/test/upload1'
